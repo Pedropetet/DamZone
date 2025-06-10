ddc21b8000bb2d193beb635477104d6bfa5a282b
Pedropetet <s1199811@student.windesheim.nl>
2025-06-10 20:09:20 +0200

    feat: add package

---
+++ DamZoneSolution/DamZoneAPI/Data/DamZoneDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DamZoneAPI.Data
+{
+    public class DamZoneDbContext : DbContext
+    {
+        public DamZoneDbContext(DbContextOptions<DamZoneDbContext> options) : base(options) { }
+
+
+    }
+}
